777f6c9140136d2dcbc82ccef2d94b67faec7da5
likang <k.li@bmi-tech.com>
2020-11-06 09:16:53 +0800

    add demo data

--- install.sh
+++ install.sh
@@ -5,15 +5,17 @@ workdir=$(cd $(dirname $0); pwd)
 # apt install ffmpeg
 # #2. create env
 # conda create -f env.yaml
-# #3. build package of DAIN
-# cd ${workdir}/DAIN/my_package
-# cp build.sh build_tmp.sh
-# sed -i "s/pytorch1.0.0/VideoPhotoRepair/g" build_tmp.sh
-# output=`./build_tmp.sh`
-# cd ../PWCNet/correlation_package_pytorch1_0
-# cp build.sh build_tmp.sh
-# sed -i "s/pytorch1.0.0/VideoPhotoRepair/g" build_tmp.sh
-# output=`./build_tmp.sh`
+#3. build package of DAIN
+cd ${workdir}/DAIN/my_package
+cp build.sh build_tmp.sh
+sed -i "s/pytorch1.0.0/VideoPhotoRepair/g" build_tmp.sh
+output=`./build_tmp.sh`
+rm build_tmp.sh
+cd ../PWCNet/correlation_package_pytorch1_0
+cp build.sh build_tmp.sh
+sed -i "s/pytorch1.0.0/VideoPhotoRepair/g" build_tmp.sh
+output=`./build_tmp.sh`
+rm build_tmp.sh
 
 cd ${workdir}/DAIN
 mkdir model_weights
